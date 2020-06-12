1cbe484a12079dabc871b6f26738ffc84cd496ff
Jesús <jrcastellano715@gmail.com>
2020-06-12 19:18:19 +0200

    estilo del log añadido al css

--- View/logs.php
+++ View/logs.php
@@ -2,13 +2,13 @@
 
 function HTMLlogs($datos){
 echo <<< HTML
-    <section>
+    <section class="informacion">
     	<div class="cabecera-log">
         	<h1>Log del sistema</h1>
         </div>
 
         <ul class="items-log">
-        	<li> <p> $datos['fecha'] <span> El usuario datos['']</span> </p></li>
+        	<li> <p> $datos['descripcion'] </p></li>
         </ul>
     </section>
 HTML;
