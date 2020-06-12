8e7acdb0efaab035dbe36112c3269ddcff36c1ab
Jesús <jrcastellano715@gmail.com>
2020-06-12 14:01:28 +0200

    añadida valoracion
    Cambiando la imagen de la valoración de la receta por elementos funcionales

--- View/receta.php
+++ View/receta.php
@@ -13,7 +13,20 @@ echo <<< HTML
         <section class="informacion">
             <div class="cabecera-receta">
                 <h1>$datos[nombre]</h1>
-                <img class="puntuacion" src='View/img/puntuacion.png' alt='puntuacion'>
+                <form class="form-Valoracion">
+                    <p class="clasificacion">
+                        <input id="radio1" type="radio" name="estrellas" value="1">
+                        <label for="radio1">★</label>
+                        <input id="radio2" type="radio" name="estrellas" value="2">
+                        <label for="radio2">★</label>
+                        <input id="radio3" type="radio" name="estrellas" value="3">
+                        <label for="radio3">★</label>
+                        <input id="radio4" type="radio" name="estrellas" value="4">
+                        <label for="radio4">★</label>
+                        <input id="radio5" type="radio" name="estrellas" value="5">
+                        <label for="radio5">★</label>
+                  </p>
+                </form>
             </div>
 
 
