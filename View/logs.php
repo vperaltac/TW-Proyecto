394a81ec2c40383fbd463841d736ad9856b30dd3
Victor Peralta <victorperalta.correo@gmail.com>
2020-06-12 21:18:02 +0200

    ahora se muestra el log por completo

--- View/logs.php
+++ View/logs.php
@@ -1,16 +1,32 @@
 <?php
 
 function HTMLlogs($datos){
-    $dato = $datos[0];
 echo <<< HTML
+<main id="bloque-principal">
     <section class="informacion">
     	<div class="cabecera-log">
         	<h1>Log del sistema</h1>
         </div>
-
         <ul class="items-log">
-        	<li> <p> $dato[descripcion] </p></li>
-        </ul>
+
+    <table style="width:100%">
+        <tr>
+            <th>Fecha</th>
+            <th>Descripción</th>
+        </tr>
+HTML;
+
+    foreach($datos as $dato){
+        echo "<tr>";
+        echo "<td>$dato[fecha]</td>";
+        echo "<td>$dato[descripcion]</td>";
+        echo "</tr>";
+    }
+
+echo <<< HTML
+    </table>
+    </ul>
     </section>
+</div>
 HTML;
 }
