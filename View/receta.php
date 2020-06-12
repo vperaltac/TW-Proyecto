5a31b900244b0f1a5b93a2ec6625ccd31713ee80
Jesús <jrcastellano715@gmail.com>
2020-06-12 14:21:20 +0200

    Arreglada la valoración
    Ahora funciona correctamente la vista de la valoración

--- View/receta.php
+++ View/receta.php
@@ -15,16 +15,18 @@ echo <<< HTML
                 <h1>$datos[nombre]</h1>
                 <form class="form-Valoracion">
                     <p class="clasificacion">
-                        <input id="radio1" type="radio" name="estrellas" value="1">
-                        <label for="radio1">★</label>
-                        <input id="radio2" type="radio" name="estrellas" value="2">
-                        <label for="radio2">★</label>
-                        <input id="radio3" type="radio" name="estrellas" value="3">
-                        <label for="radio3">★</label>
-                        <input id="radio4" type="radio" name="estrellas" value="4">
-                        <label for="radio4">★</label>
-                        <input id="radio5" type="radio" name="estrellas" value="5">
-                        <label for="radio5">★</label>
+                        <!-- Para vincular una etiqueta <label> con un elemento de entrada <input>,  el valor del atributo for de <label> tiene que ser igual al valor del atributo id del <input>
+                        Los comentarios son para que las estrellas aparezcan juntas -->
+                        <input id="radio1" type="radio" name="estrellas" value="5"><!--
+                        --><label for="radio1">★</label><!--
+                        --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+                        --><label for="radio2">★</label><!--
+                        --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+                        --><label for="radio3">★</label><!--
+                        --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+                        --><label for="radio4">★</label><!--
+                        --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+                        --><label for="radio5">★</label>
                   </p>
                 </form>
             </div>
