e559da76f65805edbfd1a105f52b125f9454e831
Jesús <jrcastellano715@gmail.com>
2020-06-13 20:51:03 +0200

    pagina para añadir comentario

--- View/nuevo_comentario.php
+++ View/nuevo_comentario.php
@@ -11,9 +11,24 @@ echo <<< HTML
 <main id="bloque-principal">
     <section class="info-contacto">
     
-    <div class="cabecera-receta">
-        <h2>Nuevo Comentario</h2>
-    </div>
+    	<div class="cabecera-receta">
+        	<h2>Tu opinión es muy importante</h2>
+    	</div>
+
+
+    	<form class="formulario" method="POST" enctype="multipart/form-data">
+        <div class="grupo-formulario">
+            
+            <label>Déjanos tu comentario :</label>
+            <textarea type="text" name="descripcion" id="new-descripcion"></textarea>
+           
+    </form>
+
+    	<div class="formulario">
+	        <input type="hidden" value="nuevo-filtro" name="peticion" />
+	        <input class="boton" type="submit" value="Comentar" name="aplicarFiltro" id="aplicarFiltro">
+        </div>
+
     </section>
 </div>
 HTML;
