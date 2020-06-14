2241dd66adc8f2c2bfa6d7fc6aa0d96da30da29f
Jesús <jrcastellano715@gmail.com>
2020-06-14 21:54:07 +0200

    añadiendo botones para paginas en listado
    se muestra una por cada elemento de la lista

--- View/listado.php
+++ View/listado.php
@@ -51,8 +51,33 @@ HTML;
                     </div>
                 </a>
             </div>
-HTML;
-    }
 
+
+              
+HTML;
+    
+    echo <<< HTML
+    <div class="paginas-listado">
+        <form action="index.php" method="post">
+            <input type="hidden" value="receta-eliminar" name="peticion" />
+            <input class="btn-eliminar-receta" id="btn-eliminar-receta" type="image" src="View/img/pprev.png" />
+        </form>
+        <form action="index.php" method="post">
+            <input type="hidden" value="receta-eliminar" name="peticion" />
+            <input class="btn-eliminar-receta" id="btn-eliminar-receta" type="image" src="View/img/rprev.png" />
+        </form>
+        <form action="index.php" method="post">
+            <input type="hidden" value="receta-eliminar" name="peticion" />
+            <input class="btn-eliminar-receta" id="btn-eliminar-receta" type="image" src="View/img/rnext.png" />
+        </form>
+        <form action="index.php" method="get">
+            <input type="hidden" value="receta-editar" name="acc" />
+            <input class="btn-editar-receta" type="image" src="View/img/pnext.png" />
+        </form>
+    </div> 
+
+    }
+HTML;
+    
     echo "</section>";
 }
